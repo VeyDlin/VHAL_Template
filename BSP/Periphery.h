5179b8b95d3dedfed45e5aa0dd5ff835dada4cab
VeyDlin <nekto@veydlin.ru>
2022-05-17 00:45:23 +0300

    ll include

--- BSP/Periphery.h
+++ BSP/Periphery.h
@@ -1,4 +1,24 @@
 #pragma once
+#include "stm32f4xx_ll_iwdg.h"
+#include "stm32f4xx_ll_rcc.h"
+#include "stm32f4xx_ll_bus.h"
+#include "stm32f4xx_ll_system.h"
+#include "stm32f4xx_ll_exti.h"
+#include "stm32f4xx_ll_cortex.h"
+#include "stm32f4xx_ll_utils.h"
+#include "stm32f4xx_ll_pwr.h"
+#include "stm32f4xx_ll_gpio.h"
 
 
+#if defined(USE_FULL_ASSERT)
+	#include "stm32_assert.h"
+#endif
 
+
+#ifndef NVIC_PRIORITYGROUP_0
+	#define NVIC_PRIORITYGROUP_0         ((uint32_t)0x00000007) 
+	#define NVIC_PRIORITYGROUP_1         ((uint32_t)0x00000006)
+	#define NVIC_PRIORITYGROUP_2         ((uint32_t)0x00000005)
+	#define NVIC_PRIORITYGROUP_3         ((uint32_t)0x00000004)
+	#define NVIC_PRIORITYGROUP_4         ((uint32_t)0x00000003)
+#endif
